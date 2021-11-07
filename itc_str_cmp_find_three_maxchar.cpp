58410159242f3eb3e9df5d93144c397ae74d361d
Maxishoo <92300834+Maxishoo@users.noreply.github.com>
2021-11-07 19:08:25 +0300

    Add files via upload

--- itc_str_cmp_find_three_maxchar.cpp
+++ itc_str_cmp_find_three_maxchar.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
-string itc_cmp_str(string str1,string str2, int num)
+string itc_cmp_str_(string str1,string str2, int num)
 {
     string neww="";
     long long lstr2=itc_len(str2);
@@ -18,14 +18,33 @@ string itc_cmp_str(string str1,string str2, int num)
             neww+=str2[i-num];
         }
     }
-    //for(long long  i=num;i<lstr2;i++)
-    //{
-        //neww+=str2[i];
-    //}
-    //neww+=str2;
-    //neww+=itc_slice_str(str1,num+lstr2,itc_len(str1));
     return neww;
 }
+
+string itc_cmp_str(string str1,string str2, int num)
+{
+    string neww="";
+    long long lstr2=itc_len(str2);
+    long long lstr1=itc_len(str1);
+    if (lstr1 < num || num < 0) return str1;
+    if(str2=="")
+    {
+        return(str1);
+    }
+    for(long long  i=0;i<lstr1;i++)
+    {
+        if (i<num ) {
+            neww+=str1[i];
+        }else if (i > (num+lstr2-1)) {
+            neww+=str1[i-lstr2];
+        }
+        else {
+            neww+=str2[i-num];
+        }
+    }
+    return neww;
+}
+
 int itc_find_str(string str1,string str2)
 {
     long long lstr1=itc_len(str1);
